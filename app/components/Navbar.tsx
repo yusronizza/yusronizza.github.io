00896dd2b21398bb6ca5d4ee8805610bfac4ad17
Yusron Izza <yusronizzafaradisa@gmail.com>
2024-08-17 17:08:49 +0700

    Update Content and Layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,10 +5,10 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
   return (
-    <nav className="bg-black text-white px-28 py-8 sticky top-0 z-50">
+    <nav className="bg-white text-black px-8 py-6 sticky top-0 z-50 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <a href="" className="text-1xl font-bold">
-          Yusron Izza
+          Curriculum Vitae
         </a>
         <div></div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,100 +8,129 @@ export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
-      <main className="flex min-h-screen flex-col items-center justify-between p-12">
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              root@yusronizza.github.io:___
-            </a>
+      <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-gray-100 text-black font-mono gap-y-8">
+      <div id="section-education" className="bg-white rounded-xl p-4 shadow-md">
+        <div className="z-10 w-full items-center justify-between">
+          <div className="flex flex-col w-full justify-center">
+            <div className="font-bold text-2xl">EDUCATION</div>
+            <div className="flex flex-col gap-y-6 py-4">
+              <div className="flex flex-row gap-x-4 w-full flex-center">
+                <div className="text-xl basis-1/8">
+                <Image
+                  src="/ehime.jpg"
+                  alt="Next.js Logo"
+                  width={110}
+                  height={110}
+                  priority
+                />
+                </div>
+                <div className="basis-3/5">
+                  <div className="text-xl font-bold">
+                    Ehime University (UGM)
+                    <div className="text-gray-500 text-sm">Matsuyama, Japan</div>
+                  </div>
+                  <div>Short Term Student Exchange Program</div>
+                  <div className="text-sm">Activities and societies: Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi, Festival Gadjah Mada 2021, Technocorner UGM, Paguyuban Karya Salemba Empat UGM, PIMNAS 35</div>
+                </div>
+                <div className="text-left basis-1/7">
+                  <div className="font-bold">
+                    October 2024 - November 2024
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row gap-x-4 w-full flex-center">
+                <div className="text-xl basis-1/8">
+                <Image
+                  src="/ugm.jpg"
+                  alt="Next.js Logo"
+                  width={110}
+                  height={110}
+                  priority
+                />
+                </div>
+                <div className="basis-3/5">
+                  <div className="text-xl font-bold">
+                    Universitas Gadjah Mada (UGM)
+                    <div className="text-gray-500 text-sm">Yogyakarta, Indonesia</div>
+                  </div>
+                  <div>Bachelor's degree, Electrical Engineering</div>
+                  <div className="text-sm">Activities and societies: Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi, Festival Gadjah Mada 2021, Technocorner UGM, Paguyuban Karya Salemba Empat UGM, PIMNAS 35</div>
+                </div>
+                <div className="text-left basis-1/7">
+                  <div className="font-bold">
+                    September 2020 - November 2024
+                  </div>
+                </div>
+              </div>
+            </div>       
           </div>
         </div>
-
-        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-          {/* <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          /> */}
+      </div>
+      <div id="section-experience" className="bg-white rounded-xl p-4 shadow-md w-full">
+        <div className="z-10 w-full items-center justify-between">
+          <div className="flex flex-col w-full justify-center">
+            <div className="font-bold text-2xl">EXPERIENCE</div>
+            <div className="flex flex-col gap-y-6 py-4">
+              <div className="flex flex-row gap-x-4 w-full flex-center">
+                <div className="text-xl basis-1/8">
+                <Image
+                  src="/Logo_Len.png"
+                  alt="Next.js Logo"
+                  width={110}
+                  height={110}
+                  priority
+                />
+                </div>
+                <div className="basis-3/5">
+                  <div className="text-md font-bold">
+                    FPGA Engineer
+                    <div className="text-gray-500 text-sm">Bandung, Indonesia</div>
+                  </div>
+                  <div>PT Len Industri (Persero)</div>
+                  <div className="text-sm">Job Description:
+                    <ul className="list-disc list-inside text-justify">
+                      <li>Used ADRV9364-Z7020 All Programmable System on Module (SOM)</li>
+                      <li>Implemented Amplitude Modulation (AM) using Embedded Linux in C Programming Language</li>
+                      <li>Used MATLAB and Simulink to Model a System and HDL Coder to generate Verilog Code</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="text-left basis-1/7 font-bold">
+                February 2024 - June 2024
+                </div>
+              </div>
+              <div className="flex flex-row gap-x-4 w-full flex-center">
+                <div className="text-xl basis-1/8">
+                <Image
+                  src="/ugm_career.jpg"
+                  alt="Next.js Logo"
+                  width={110}
+                  height={110}
+                  priority
+                />
+                </div>
+                <div className="basis-3/5">
+                  <div className="text-md font-bold">
+                    Digital Signal Processing Junior Project Lab Work Assistant
+                    <div className="text-gray-500 text-sm">Yogyakarta, Indonesia</div>
+                  </div>
+                  <div>Universitas Gadjah Mada</div>
+                  <div className="text-sm">Job Description
+                    <ul className="list-disc list-inside">
+                      <li>Unit 1: Frequency Spectrum and Filter Implementation using Python</li>
+                      <li>Unit 2: Finite Impulse Response Implementation and Windowing using Python</li>
+                      <li>Unit 3: Digital to Analog Converter and Analog to Digital Converter Implementation in STM32 Nucleo-F446RE Development Board Using Timer.</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="text-right basis-1/7 font-bold">
+                  September 2020 - November 2024
+                </div>
+              </div>
+            </div>       
+          </div>
         </div>
-
-        <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Projects{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Education{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Services{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Explore starter templates for Next.js.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Deploy{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div>
+      </div>
       </main>
       <Footer></Footer>
     </div>
