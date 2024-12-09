e346c4c0055af6d5cd69e9bdb1961a0bd9a76f7a
Yusron Izza <yusronizzafaradisa@gmail.com>
2024-12-09 23:55:11 +0700

    Update Homepage

--- app/Skills.tsx
+++ app/Skills.tsx
@@ -3,11 +3,30 @@ import Image from "next/image";
 export const Skills = () => {
     const image_container = "flex justify-center items-center"
     return(
-        <div className="bg-white rounded-md p-4 shadow-md ml-80 w-half">
+        <div className="bg-white rounded-md p-4 shadow-md ml-80 w-2/3">
         <div className="z-10 w-full items-center justify-between">
-          <div className="flex flex-col w-full justify-center items-center">
-            <div className="font-bold text-2xl text-center">SKILLS</div>
-            <div className="font-bold text-gray-600 text-sm text-center py-2">Programming Language & Digital Design</div>
+          <div className="flex flex-col w-full">
+            <div className="font-bold text-2xl">Skills</div>
+            
+            <div className="font-bold text-gray-600 text-sm py-2">Programming Language & Digital Design</div>
+            <div className="flex flex-row gap-x-4 py-2 items-center">
+              <div className="w-1/7">
+                <Image
+                  src="/programming/c.png"
+                  alt="Next.js Logo"
+                  width={35}
+                  height={35}
+                  priority
+                />
+              </div>
+              <div className="w-full">
+                <div>C</div>
+                <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                  <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: "45%" }}> 45%</div>
+                </div>
+              </div>
+            </div>
+            
             <div className="flex flex-row gap-x-8 my-4">
               <div className="flex flex-col ">
                 <div className={image_container}>
--- app/page.tsx
+++ app/page.tsx
@@ -16,62 +16,99 @@ export default function Home() {
     <div className={myFont.className}>
       <Navbar></Navbar>
       <main className="flex min-h-screen flex-col items-center justify-between py-8 px-36 bg-gray-100 text-black gap-y-8">
-      <div className="left-36 fixed z-50 flex flex-col md:flex-row justify-between p-4 bg-white border border-gray-100 rounded-md shadow-md lg:max-w-7xl">
-          <div className="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
-              <div className="text-center items-center ">
-                <div className={image_container}>
-                  <Image
-                    src="/Profile.png"
-                    alt="Yusron Izza F"
-                    width={150}
-                    height={150}
-                    priority
-                  />
-                </div>
-                <div className="font-bold text-xl">Yusron Izza Faradisa</div>
-                <div className="">Bachelor of Electrical Engineering</div>
-                <div className="">Yogyakarta, Indonesia</div>
-                <div className="flex flex-row mt-2 gap-x-6 text-base items-center text-center justify-center">
-                  <a href="">
-                    <div className="flex items-center gap-2">
-                      <FaMap className="text-xl" />
-                    </div>
-                  </a>
-                  <a href="https://www.linkedin.com/in/yusronizza">
-                    <div className="flex items-center gap-2">
-                      <FaLinkedin className="text-2xl" />
-                    </div>
-                  </a>
-                  <a href="https://www.github.com/yusronizza">
-                    <div className="flex items-center gap-2">
-                      <FaGithub className="text-2xl" />
-                    </div>
-                  </a>
-                  <a href="https://www.instagram.com/yusronizza">
-                    <div className="flex items-center gap-2">
-                      <FaInstagram className="text-2xl" />
-                    </div>
-                  </a>
-                </div>
-              </div>
-          </div>
-      </div>
-      <div className="bg-white rounded-md p-4 shadow-md ml-80">
-        <div className="z-10 w-full items-center justify-between sticky">
-          <div className="flex flex-col w-full">
-            <div className="font-bold text-2xl">About Me</div>
-            <div className={section_center}>
-                Yusron Izza F is an undergraduate electrical engineering student at Universitas Gadjah Mada. He loves science and technology especially in electronics, computer, and programming. He has a strong foundation in computer programming, electronics design, and computer architecture. He likes to work in groups and lead groups. 
+      <div className="lg:left-36 w-96 lg:w-2/12 lg:fixed z-10 flex flex-col md:flex-row justify-center p-4 bg-white border border-gray-100 rounded-md drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]">
+        <div className="flex flex-col items-start mb-3 items-center md:flex-row md:mb-0">
+          <div className="text-center items-center ">
+            <div className={image_container}>
+              <Image
+                src="/Profile.png"
+                alt="Yusron Izza F"
+                width={100}
+                height={100}
+                className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]"
+                priority
+              />
             </div>
-            <div>Area of Expertise</div>
-          </div>
-          <div className="flex gap-x-2">
-            <a href="cv/cv.pdf" className="bg-gray-400 hover:bg-gray-600 text-white p-2 px-4 rounded-2xl">Download CV</a>
-            <a href="mailto:yusronizzafaradisa@gmail.com" className="bg-gray-400 hover:bg-gray-600 text-white p-2 px-4 rounded-2xl">Contact Me</a>
+            <div className="font-bold text-xl">Yusron Izza Faradisa</div>
+            <div className="">Bachelor of Electrical Engineering</div>
+            <div className="">Yogyakarta, Indonesia</div>
+            <div className="mt-3">Area of Interest</div>
+            <div className="w-full bg-gray-100 p-2 rounded-md text-sm">
+              Integrated Circuit Design
+            </div>
+            <div className="w-full bg-gray-100 p-2 rounded-md mt-2 text-sm">
+              Embedded System
+            </div>
+            <div className="w-full bg-gray-100 p-2 rounded-md mt-2 mb-5 text-sm">
+              Machine Learning
+            </div>
+            <div className="flex flex-row mt-2 gap-x-6 text-base items-center text-center justify-center">
+              <a href="">
+                <div className="flex items-center gap-2">
+                  <FaMap className="text-xl" />
+                </div>
+              </a>
+              <a href="https://www.linkedin.com/in/yusronizza">
+                <div className="flex items-center gap-2">
+                  <FaLinkedin className="text-2xl" />
+                </div>
+              </a>
+              <a href="https://www.github.com/yusronizza">
+                <div className="flex items-center gap-2">
+                  <FaGithub className="text-2xl" />
+                </div>
+              </a>
+              <a href="https://www.instagram.com/yusronizza">
+                <div className="flex items-center gap-2">
+                  <FaInstagram className="text-2xl" />
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="bg-white rounded-md p-4 shadow-md ml-80">
+      <div className="flex flex-row">
+        <div className="lg:w-3/12"></div>
+        <div className="lg:w-9/12 flex flex-col gap-y-6">
+        <div className="w-96 md:w-full bg-white rounded-md p-4 drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]">
+          <div className="z-10 w-full items-center justify-center">
+            <div className="flex flex-col w-full">
+              <div className="font-bold text-2xl">About Me</div>
+              <div className={section_center}>
+                  Yusron Izza F is an undergraduate electrical engineering student at Universitas Gadjah Mada. He loves science and technology especially in electronics, computer, and programming. He has a strong foundation in computer programming, electronics design, and computer architecture. He likes to work in groups and lead groups. 
+              </div>
+              <div className="font-bold">Area of Expertise</div>
+              <div className="flex flex-col md:flex-row gap-x-4 py-2 mb-2">
+                <div className="flex w-full bg-white rounded-md p-2 gap-x-2 outline outline-offset-2 outline-1 outline-gray-300 shadow-sm">
+                  <div className="">Logo</div>
+                  <div className="">
+                    <span className="font-bold text-sm">Embedded System</span>
+                    <p>Creating electronics design</p>
+                  </div>
+                </div>
+                <div className="flex w-full bg-white rounded-md p-2 gap-x-2 outline outline-offset-2 outline-1 outline-gray-300 shadow-sm">
+                  <div className="">Logo</div>
+                  <div className="">
+                    <span className="font-bold text-sm">Electronics Design</span>
+                    <p>Creating electronics design</p>
+                  </div>
+                </div>
+                <div className="flex w-full bg-white rounded-md p-2 gap-x-2 outline outline-offset-2 outline-1 outline-gray-300 shadow-sm">
+                  <div className="">Logo</div>
+                  <div className="">
+                    <span className="font-bold text-sm">ASIC</span>
+                    <p>Creating electronics design</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-2">
+              <a href="cv/cv.pdf" className="bg-gray-400 hover:bg-gray-600 text-white p-2 px-4 rounded-2xl">Download CV</a>
+              <a href="mailto:yusronizzafaradisa@gmail.com" className="bg-gray-400 hover:bg-gray-600 text-white p-2 px-4 rounded-2xl">Contact Me</a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-md p-4 drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]">
         <div className="z-10 w-full items-center justify-between sticky">
           <div className="flex flex-col w-full">
             <div className="font-bold text-2xl">Education</div>
@@ -93,7 +130,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-white rounded-md p-4 shadow-md ml-80">
+      <div className="w-full bg-white rounded-md p-4 drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]">
         <div className="z-10 w-full items-center justify-between">
           <div className="flex flex-col">
             <div className="font-bold text-2xl">Experiences</div>
@@ -101,7 +138,7 @@ export default function Home() {
                 <li className="ms-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time className="mb-1 text-sm font-normal leading-none">January  2024 - June 2024</time>
-                    <h3 className="text-lg font-semibold text-gray-900">FPGA Engineer (Intership)</h3>
+                    <h3 className="text-lg font-semibold text-gray-900">FPGA Engineer (Internship)</h3>
                     <h4 className="text-sm text-gray-500">PT Len Industri (Persero) - Bandung, Rep. of Indonesia</h4>
                     <p className="mb-4 text-base text-md">Developed radio communication modulation using Binary Phase Shift Keying and Quadrature Phase Shift Keying in Verilog HDL.</p>
                 </li>
@@ -110,13 +147,80 @@ export default function Home() {
                     <time className="mb-1 text-sm font-normal leading-none">October 2023 - December 2023</time>
                     <h3 className="text-lg font-semibold text-gray-900">Digital Signal Processing Lab Work Assistant</h3>
                     <h4 className="text-sm text-gray-500">Universitas Gadjah Mada - Yogyakarta, Rep. of Indonesia</h4>
-                    <p className="text-base text-md">Led session in Lab Work Practice: Frequency Spectrum and Filter Implementation using Python; Finite Impulse Response Implementation and Windowing using Python; Digital to Analog Converter and Analog to Digital Converter Implementation in STM32 Nucleo-F446RE Development Board Using Timer.</p>
+                    <p className="mb-4 text-base text-md">Led session in Lab Work Practice: Frequency Spectrum and Filter Implementation using Python; Finite Impulse Response Implementation and Windowing using Python; Digital to Analog Converter and Analog to Digital Converter Implementation in STM32 Nucleo-F446RE Development Board Using Timer.</p>
+                </li>
+                <li className="ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none">September 2023 - December 2023</time>
+                    <h3 className="text-lg font-semibold text-gray-900">Basic Electronics Lab Work Assistant</h3>
+                    <h4 className="text-sm text-gray-500">Universitas Gadjah Mada - Yogyakarta, Rep. of Indonesia</h4>
+                    <p className="mb-4 text-base text-md">Led session in Lab Work Practice: Instrumentation Amplifier; AC-DC Converter Regulator; Unit 3: Phase Shift Oscillator.</p>
+                </li>
+                <li className="ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none">August 2023 - December 2023</time>
+                    <h3 className="text-lg font-semibold text-gray-900">Data Communication and Network Lab Work Assistant</h3>
+                    <h4 className="text-sm text-gray-500">Universitas Gadjah Mada - Yogyakarta, Rep. of Indonesia</h4>
+                    <p className="mb-4 text-base text-md">Led session in Lab Work Practice: Asynchronous Serial Communication; Digital Modulation; Unit 3: Digital Communication.</p>
+                </li>
+                <li className="ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none">January 2023 - February 2023</time>
+                    <h3 className="text-lg font-semibold text-gray-900">Instrumentation and Control Engineer (Internship)</h3>
+                    <h4 className="text-sm text-gray-500">PT Nusa Halmahera Minerals - North Halmahera Regency, North Maluku, Rep. of Indonesia</h4>
+                    <p className="mb-4 text-base text-md">Developed PID value calculator from the SCADA historic data controller performance.</p>
                 </li>
             </ol>
           </div>
         </div>
       </div>
-      <Skills></Skills>
+      <div className="bg-white w-full rounded-md p-4 drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]">
+        <div className="z-10 w-full items-center justify-between">
+          <div className="flex flex-col">
+            <div className="font-bold text-2xl">Skills</div>
+            <div className="font-bold text-gray-600 text-sm py-2">Programming Language</div>
+            <div className="flex flex-col md:flex-row gap-x-12 py-2 items-center">
+            <div className="w-1/2 flex flex-row py-2 items-center">
+              <div className="w-2/12">
+                <Image
+                  src="/programming/c.png"
+                  alt="Next.js Logo"
+                  width={35}
+                  height={35}
+                  priority
+                />
+              </div>
+              <div className="w-10/12">
+                <span className="font-bold">C</span>
+                <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                  <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: "60%" }}> 60%</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/2 flex flex-row py-2 items-center">
+              <div className="w-2/12">
+                <Image
+                  src="/programming/Python.png"
+                  alt="Next.js Logo"
+                  width={35}
+                  height={35}
+                  priority
+                />
+              </div>
+              <div className="w-10/12">
+                <span className="font-bold">Python</span>
+                <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                  <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: "80%" }}> 80%</div>
+                </div>
+              </div>
+            </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
       </main>
       <Footer></Footer>
     </div>
